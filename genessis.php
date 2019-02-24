38852a606670da6723dcc6c0baf16c64f850f9bd
mander39 <39866682+mander39@users.noreply.github.com>
2019-02-24 18:12:42 -0500

    Coder Requirements
    > Slider installed
    > HTML to PHP w/ incs
    > Nesting cleanup

---
+++ genessis.php
@@ -0,0 +1,38 @@
+<?php include 'inc/html-top01.inc'; ?>
+<title>Lab 05 | Genessis Galindo</title>
+<?php include 'inc/html-top02.inc'; ?>
+		
+<header>
+	<h1>Genessis Galindo</h1>
+	<img src="images/genessis.jpg" alt="Genessis Galindo">
+</header>
+
+<?php include 'inc/html-nav.inc'; ?>
+
+<main>
+	<h2>Home Country</h2>
+		<div>United States</div>
+
+	<h2>Hometown</h2>
+		<div>Los Angeles, CA</div>
+
+	<h2>Pre-College Life</h2>
+		<p>Before coming to the University of Rochester, I attended Canyon High School in California where I was involved in various extracurricular such as: Student Government, National Honor Society, English National Honor Society, Spirit Club, Chemistry Club, and a few others. I even founded a chapter of the Science National Honor Society at my school and at the time I believed I wanted to major in Chemistry.</p>
+
+	<h2>Focus of Study</h2>
+		<p>Major in Business, Minor in Computer Science and Spanish.</p>
+
+	<h2>College Life</h2>
+		<p>Since starting college, I have held 3 jobs during the academic term. My first ever job at UR was at the Common Market where I worked for about a year and a half before deciding that I wanted a change. I applied to the Greene Center where I now work as a Front Desk/Administrative Assistant. I really enjoy working at the Greene Center because I am not only able to help people discover resources available to them on campus, but I also am able to develop my professional skills throughout this role. I also am currently working on a startup called BarterOut, which is a textbook exchange platform for students by students, as a Marketing Analyst.</p>
+
+	<h2>Extra-Curriculars</h2>
+		<ul>
+			<li>Student Association</li>
+			<li>Alpha Kappa Psi</li>
+			<li>Gamma Phi Beta</li>
+			<li>Student Programming Board</li>
+			<li>2020 Class Council</li>
+		</ul>
+</main>
+
+<?php include 'inc/html-bottom.inc'; ?>
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php include 'inc/html-top01.inc'; ?>
+<title>Lab 05 | Home</title>
+<?php include 'inc/html-top02.inc'; ?>
+<?php include 'inc/html-nav.inc'; ?>
+<?php include 'inc/html-slider.inc'; ?>
+
+<a href="genessis.html">
+    <h2>Genessis Galindo</h2>
+    <img src="images/genessis.jpg" alt="Genessis Galindo">
+    <h3>Home Country</h3>
+    <div>United States</div>
+</a>
+
+<a href="leo.html">
+    <h2>Leonard Koblence</h2>
+    <img src="images/leo.jpg" alt="leo">
+    <h3>Home Country</h3>
+    <div>United States</div>
+</a>
+
+<a href="qianwen.html">
+    <h2>Qianwen Fu</h2>
+    <img src="images/qianwen.jpeg" alt="a picture of Qianwen Fu">
+    <h3>Home Country</h3>
+    <div>China</div>
+</a>
+
+<?php include 'inc/html-bottom.inc'; ?>
