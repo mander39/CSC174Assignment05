93678eb9a403f7b974688043abc98e83f9c47b5a
Amanda <amanda.k.odonnell@gmail.com>
2019-02-26 02:44:41 -0500

    1st pass, not nearly finished
    don't worry ik this is not nearly enough css and that it's still very rough I just wanted to upload this stuff so I don't loose the progress

--- index.php
+++ index.php
@@ -4,21 +4,21 @@
 <?php include 'inc/html-nav.inc'; ?>
 <?php include 'inc/html-slider.inc'; ?>
 
-<a href="genessis.php">
+<a href="genessis.php" id="genessis">
     <h2>Genessis Galindo</h2>
     <img src="images/genessis.jpg" alt="Genessis Galindo">
     <h3>Home Country</h3>
     <div>United States</div>
 </a>
 
-<a href="leo.php">
+<a href="leo.php" id="leonard">
     <h2>Leonard Koblence</h2>
     <img src="images/leo.jpg" alt="leo">
     <h3>Home Country</h3>
     <div>United States</div>
 </a>
 
-<a href="qianwen.php">
+<a href="qianwen.php" id="qianwen">
     <h2>Qianwen Fu</h2>
     <img src="images/qianwen.jpeg" alt="a picture of Qianwen Fu">
     <h3>Home Country</h3>
