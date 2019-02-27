551cb0636ddfa3d1219c1d1e6801c5f39f6555d7
Amanda <amanda.k.odonnell@gmail.com>
2019-02-27 01:35:21 -0500

    more css not done yet just wanted to incorporate new changes

--- index.php
+++ index.php
@@ -2,27 +2,28 @@
 <title>Lab 05 | Home</title>
 <?php include 'inc/html-top02.inc'; ?>
 <?php include 'inc/html-nav.inc'; ?>
-<?php include 'inc/html-slider.inc'; ?>
 
-<a href="genessis.php" id="genessis">
-    <h2>Genessis Galindo</h2>
-    <img src="images/genessis.jpg" alt="Genessis Galindo">
-    <h3>Home Country</h3>
-    <div>United States</div>
-</a>
+    <?php include 'inc/html-slider.inc'; ?>
 
-<a href="leo.php" id="leonard">
-    <h2>Leonard Koblence</h2>
-    <img src="images/leo.jpg" alt="leo">
-    <h3>Home Country</h3>
-    <div>United States</div>
-</a>
+    <a href="genessis.php" id="genessis" class="america">
+        <h2>Genessis Galindo</h2>
+        <img src="images/genessis.jpg" alt="Genessis Galindo">
+        <h3>Home Country</h3>
+        <div>United States</div>
+    </a>
 
-<a href="qianwen.php" id="qianwen">
-    <h2>Qianwen Fu</h2>
-    <img src="images/qianwen.jpeg" alt="a picture of Qianwen Fu">
-    <h3>Home Country</h3>
-    <div>China</div>
-</a>
+    <a href="leo.php" id="leonard" class="america">
+        <h2>Leonard Koblence</h2>
+        <img src="images/leo.jpg" alt="leo">
+        <h3>Home Country</h3>
+        <div>United States</div>
+    </a>
+
+    <a href="qianwen.php" id="qianwen" class="china">
+        <h2>Qianwen Fu</h2>
+        <img src="images/qianwen.jpeg" alt="a picture of Qianwen Fu">
+        <h3>Home Country</h3>
+        <div>China</div>
+    </a>
 
 <?php include 'inc/html-bottom.inc'; ?>
